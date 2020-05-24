3f9e733bdaba50cd28e9a9a0f32df4bf85b84d2d
stasundr <stasundr@gmail.com>
2020-05-24 18:54:20 +0700

    hostname

--- backend/main.go
+++ backend/main.go
@@ -1,12 +1,17 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
+	hostname, _ := os.Hostname()
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message": "pong",
+			"message": "pong v0.0.2 " + hostname,
 		})
 	})
 	r.Run() // listen and serve on 0.0.0.0:8080
